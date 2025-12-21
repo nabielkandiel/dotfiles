a1c366f707726c37fa7e16f7ecbdfc4a4a711920
Nabiel Kandiel <nabkan5@gmail.com>
2025-12-21 12:37:13 -0500

    maybe fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,33 @@
 return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     ---@diagnostic disable-next-line: missing-fields
-    require('nvim-treesitter.configs').setup {
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'dart',
-        'diff',
-        'go',
-        'html',
-        'javascript',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'query',
-        'rust',
-        'typescript',
-        'vim',
-        'vimdoc',
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    }
-  end,
+    require('nvim-treesitter').setup({
+        ensure_installed = {
+            'bash',
+            'c',
+            'cpp',
+            'diff',
+            'html',
+            'lua',
+            'luadoc',
+            'markdown',
+            'markdown_inline',
+            'python',
+            'query',
+            'rust',
+            'vim',
+            'vimdoc',
+        },
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = { 'ruby' },
+        },
+        indent = { enable = true, disable = { 'ruby' } },
+    })
+    end,
 }
